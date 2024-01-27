28e72414815f198bb51fec9bfa66c86187b21d26
Alexander Nyberg <alexandernyberg@me.com>
2024-01-27 16:37:57 +0100

    Implementing split text
    Implementing split by segments to make it easier to sort out
    unnecessary information.

--- getData.py
+++ getData.py
@@ -7,7 +7,16 @@ def getPinchosLunches():
     lunches = []
     url = "https://rskrapan.se/"
     headers = {"User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" }
-    r = httpx.get(url, headers=headers)
+    
+    try:
+        r = httpx.get(url, headers=headers)
+        r.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e}")
+        return
+    except httpx.RequestError as e:
+        print(f"An error occurred while requesting {e.request.url!r}.")
+        return
     
     soup = BeautifulSoup(r.text, 'html.parser')
     meals = soup.find('div', {"class": "main-page"}).find_all('h3')
@@ -20,11 +29,21 @@ def getPinchosLunches():
     with open('pinchos-lunches.json', 'w') as f:
         json.dump(lunches, f)
 
+
 def getSkafferietLunches():
     lunches = []
     url = "https://www.skafferietlidkoping.se/"
     headers = {"User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" }
-    r = httpx.get(url, headers=headers)
+    
+    try:
+        r = httpx.get(url, headers=headers)
+        r.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e}")
+        return
+    except httpx.RequestError as e:
+        print(f"An error occurred while requesting {e.request.url!r}.")
+        return
     
     soup = BeautifulSoup(r, 'html.parser')
     meals = soup.find('div', {"class": "postdiv"}).find_all('h6')
@@ -37,9 +56,35 @@ def getSkafferietLunches():
     with open('skafferiet-lunches.json', 'w') as f:
         json.dump(lunches, f)
 
+
+def getMellbyGatans():
+    lunches = []
+    url = "https://www.mellbygatans.se/sv/default"
+    headers = {"User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" }
+
+    try:
+        r = httpx.get(url, headers=headers)
+        r.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e}")
+        return
+    except httpx.RequestError as e:
+        print(f"An error occurred while requesting {e.request.url!r}.")
+        return
+        
+    soup = BeautifulSoup(r, 'html.parser')
+    meals = soup.find('div', {"class": "week-menu"}).find_all("p")
+    
+    print('MELLBYGATANS')
+    for meal in meals:
+       lunches.append(meal.text)
+       print("Getting:", meal.text)
+
+    with open('mellbygatans-lunches.json', 'w', encoding="utf-8") as f:
+        json.dump(lunches, f,indent=4)
+
 def main():
-    getPinchosLunches()
-    getSkafferietLunches()
+    getMellbyGatans()
 
 if __name__ == "__main__":
     main()
