08cb704a0a42a593c0c8af3c44189b680672757d
Alexander Nyberg <alexandernyberg@me.com>
2024-02-13 17:41:57 +0100

    Adding model to models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,3 +4,11 @@ MELLBYGATANS_JSON = './json/mellbygatans.json'
 SKAFFERIET_JSON = './json/skafferiet.json'
 PINCHOS_JSON = './json/pinchos.json'
 VILLARESTAURANGEN_JSON = './json/villa-restaurangen.json'
+
+
+def readMellbygatans():
+    with open(MELLBYGATANS_JSON, 'r', encoding='utf-8') as file:
+        menu_data = json.load(file)
+    for item in menu_data:
+        print(item)
+
