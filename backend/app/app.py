22d8bd3068c95d12793aae8c20338ba1af964f14
Alexander Nyberg <alexandernyberg@me.com>
2024-02-13 17:37:42 +0100

    Cleaning up
    Cleaned up the frontend directory and added a models.py to the backend
    file structure so there's a dedicated place for setting up the models
    for each restaurant.

--- backend/app/app.py
+++ backend/app/app.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get("/")
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
 
---
+++ backend/app/models.py
@@ -0,0 +1,6 @@
+import json
+
+MELLBYGATANS_JSON = './json/mellbygatans.json'
+SKAFFERIET_JSON = './json/skafferiet.json'
+PINCHOS_JSON = './json/pinchos.json'
+VILLARESTAURANGEN_JSON = './json/villa-restaurangen.json'
