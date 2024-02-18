f154453ec62e1c60b1797c076aa2dda91035ed2c
Alexander Nyberg <alexandernyberg@me.com>
2024-02-18 18:16:57 +0100

    Added navbar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import FetchAllMenus from './MenuCards';
+import Navbar from './Navbar';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Homepage</h1>
-      <p>This is the homepage of our website.</p>      
-      <div>
-        <main>
-          <FetchAllMenus />
-        </main>
+  <div>
+    <Navbar />
+    <div className='container'>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column'>
+            <h1 className='title is-1 has-text-centered'>Restaurangmenyer</h1>
+          </div>
+        </div>
+        <div className='container'>
+          <main>
+            <FetchAllMenus />
+          </main>
+        </div>
       </div>
     </div>
+  </div>
   );
 };
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <h1 className="title is-3">LUNCH LIDKÖPING</h1>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
