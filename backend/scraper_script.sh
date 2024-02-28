48d9b7f47ba7f98bc627873b6d1e3ccd808136d2
Alexander Nyberg <alexandernyberg@me.com>
2024-02-28 15:18:19 +0100

    Trying to figure out why cron job isn't running as it should

--- backend/scraper_script.sh
+++ backend/scraper_script.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-echo "Cron job started"  # Output will be logged
-python update_data.py >> /var/log/update_data.log 2>&1
+echo "Cron job started - Fetching Menu Data"  # Output will be logged
 
 # Change directory to where your Python script is located
 cd /backend
 
 # Run your Python web scraping script
-python3 fetch_menu_data.py
+python3 fetch_menu_data.py > /var/log/fetch_menu_data.log 2>&1
